075b411e2573b8feb5b4a21cd0c16fc8154f2ec9
Nicola De Cao <ndecao@fb.com>
2020-06-10 10:54:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="fairkilt",  # Replace with your own username
+    name="kilt",  # Replace with your own username
     version="0.0.1",
     author="Fabio Petroni",
     author_email="fabiopetroni@fb.com",
