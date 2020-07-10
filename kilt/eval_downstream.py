c72829a5864c84ba8b488e582fd1b853e3810749
fabiopetroni <fabio.petroni@fb.com>
2020-07-10 05:42:18 -0700

    fix

--- kilt/eval_downstream.py
+++ kilt/eval_downstream.py
@@ -217,6 +217,7 @@ def validate_input(gold_records, guess_records):
 
     id2guess_record = {}
     for guess in guess_records:
+        assert str(guess["id"]).strip() not in id2guess_record
         id2guess_record[str(guess["id"]).strip()] = guess
 
     guess_records = []
